9d2e5508821800b31e7029d94931edec7eb580b5
Alex Quarrie <144243422+alexquar@users.noreply.github.com>
2024-10-08 13:49:05 -0400

    Add files via upload

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Alex on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
