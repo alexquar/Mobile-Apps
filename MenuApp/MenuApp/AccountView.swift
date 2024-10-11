06b9ccefecc464ffd14efbca08ca19a7a741d77d
Alex Quarrie <144243422+alexquar@users.noreply.github.com>
2024-10-11 15:12:50 -0400

    Add files via upload

---
+++ MenuApp/MenuApp/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  MenuApp
+//
+//  Created by Alex on 2024-10-11.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text("Account View")
+    }
+}
+
+#Preview {
+    AccountView()
+}
---
+++ MenuApp/MenuApp/AppetizerListView.swift
@@ -0,0 +1,23 @@
+//
+//  AppetizerListView.swift
+//  MenuApp
+//
+//  Created by Alex on 2024-10-11.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text("List View")
+            } .navigationTitle("Appetizer List View")
+
+        }
+    }
+}
+
+#Preview {
+    AppetizerListView()
+}
