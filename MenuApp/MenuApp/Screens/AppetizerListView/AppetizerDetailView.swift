f830a181b57c5754375882c68bd3308d83ec3bf5
Alex Quarrie <144243422+alexquar@users.noreply.github.com>
2024-10-13 19:06:13 -0400

    Add files via upload

---
+++ MenuApp/MenuApp/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AppetizerDetailView.swift
+//  MenuApp
+//
+//  Created by Alex on 2024-10-13.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    let appetizer:Appetizer
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+}
