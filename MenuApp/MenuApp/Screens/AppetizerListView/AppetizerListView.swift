edc1712824f78d14329f58714e093b31a91646c2
Alex Quarrie <144243422+alexquar@users.noreply.github.com>
2024-10-13 21:01:18 -0400

    Add files via upload

---
+++ MenuApp/MenuApp/Screens/AppetizerListView/AppetizerListView.swift
@@ -0,0 +1,63 @@
+//
+//  AppetizerListView.swift
+//  MenuApp
+//
+//  Created by Alex on 2024-10-11.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    
+    
+    //view model instance
+    @StateObject var viewModel = AppetizerListViewModel()
+    
+    var body: some View {
+        ZStack{
+            NavigationView{
+                List(viewModel.appetizers){ appetizer in
+                    AppetizerView(appetizer: appetizer)
+                }.navigationTitle("Appetizers")
+            }  
+            .onAppear{
+                viewModel.getAppetizers()
+            }
+            if viewModel.isLoading{
+                LoadingView()
+            }
+        }.alert(item: $viewModel.alertItem, content: { alertItem in
+            Alert(
+                title: alertItem.title,
+                message: alertItem.message,
+                dismissButton: alertItem.dismissButton
+            )
+        })
+    }
+}
+
+#Preview {
+    AppetizerListView()
+}
+
+struct AppetizerView: View {
+    var appetizer:Appetizer
+    var body: some View {
+        HStack{
+            //get image through our appetizer remote image and the url we get from the reqs
+            AppetizerRemoteImage(url: appetizer.imageURL)
+                .aspectRatio(contentMode: .fit)
+                .frame(width:120, height:90)
+                .cornerRadius(10)
+            VStack(alignment: .leading, spacing:5){
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                Text("$\(appetizer.price, specifier:"%.2f")")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.secondary)
+            }.padding(.leading)
+            
+        }
+    }
+}
